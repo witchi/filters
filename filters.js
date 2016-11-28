448b772f10359b457339dc1d891490111bc31d6a
Artur Petrov <admin@gtn18.ru>
2016-11-28 21:55:36 +0400

    Fixed for Roundcube 1.2.2

--- filters.js
+++ filters.js
@@ -3,7 +3,7 @@
  *
  * Plugin that adds a new tab to the settings section to create client-side e-mail filtering.
  *
- * @version 2.1.4
+ * @version 2.1.5
  * @author Roberto Zarrelli <zarrelli@unimol.it>
  * @developer Artur Petrov <admin@gtn18.ru>
  */
